b10565e627bd53b351672e22dbe70a094f8c3192
Ada1306 <149617245+Ada1306@users.noreply.github.com>
2024-02-28 02:02:46 +0100

    Add files via upload
    Updated

--- js/modal.js
+++ js/modal.js
@@ -5,4 +5,4 @@ const modalBtnClose = document.querySelector('.modal-btn-close');
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);s
+modalBtnClose.addEventListener('click', toggleModal);
